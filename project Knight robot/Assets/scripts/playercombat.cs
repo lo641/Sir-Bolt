3c0e958e899833c0b8a57674f0dbe2fa7b5630df
lo641 <lcooper1955@marist.vic.edu.au>
2022-10-18 12:36:56 +1100

    A 2D platformer Adventure game

---
+++ project Knight robot/Assets/scripts/playercombat.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercombat : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Attack(); 
+        }
+    } 
+     
+
+    void Attack()
+    {
+        // play an Attack animation  
+        animator.SetTrigger("Attack");
+        // Detact enemies in range of attack 
+
+        // Damage them
+       
+    }
+
+    void OnDrawGizmosSelected()
+    { 
+        if(attackPoint == null) 
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
