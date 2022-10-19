0826a17d07cdec96d49cc070d975cebe6df5b53e
lo641 <lcooper1955@marist.vic.edu.au>
2022-10-20 09:53:37 +1100

    projectile behaveiour / arm boom rang

---
+++ project Knight robot/Assets/scripts/projectile behaveiour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectilebehaveiour : MonoBehaviour
+{
+    public float speed = 4.5f;
+    private void Update()
+    {
+        transform.position += transform.right * Time.deltaTime * speed; 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
